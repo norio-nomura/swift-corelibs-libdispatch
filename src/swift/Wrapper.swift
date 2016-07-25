0821ef88bea06c0304023f7d602a2a4ab0391984
Matt Wright <mww@apple.com>
2016-07-25 12:22:18 -0700

    @noreturn -> Never

--- src/swift/Wrapper.swift
+++ src/swift/Wrapper.swift
@@ -15,7 +15,7 @@ import CDispatch
 // This file contains declarations that are provided by the
 // importer via Dispatch.apinote when the platform has Objective-C support
 
-@noreturn public func dispatchMain() {
+public func dispatchMain() -> Never {
 	CDispatch.dispatch_main()
 }
 
